c63a0021a4b6fe4257aaaf06687ed1aaaef51a4a
yuni0725 <yuni0725@naver.com>
2024-05-24 22:56:15 +0900

    Update to 0.0.2
    Add Week2

--- src/routes/task-menu.tsx
+++ src/routes/task-menu.tsx
@@ -21,24 +21,34 @@ const Wrapper = styled.div`
 `;
 
 export default function TaskMenu() {
-  const [taskOfList, setTaskOfList] = useState<TaskType[]>([]);
-  const getTaskData = async () => {
-    const TaskQuery = query(collection(db, "week1"));
+  const [taskOfListFirst, setTaskOfListFirst] = useState<TaskType[]>([]);
+  const [taskOfListSecond, setTaskOfListSecond] = useState<TaskType[]>([]);
+  //const [taskOfListThird, setTaskOfListThird] = useState<TaskType[]>([]);
+  const getTaskData = async (week_num: number) => {
+    const TaskQuery = query(collection(db, `week${week_num}`));
     const snapshot = await getDocs(TaskQuery);
     const tasks = snapshot.docs.map((doc) => {
       const { difficulty, fileID, fileName, readOnly } = doc.data();
       return { difficulty, fileID, fileName, readOnly };
     });
-    setTaskOfList(tasks);
+    if (week_num === 1) {
+      setTaskOfListFirst(tasks);
+    } else if (week_num === 2) {
+      setTaskOfListSecond(tasks);
+    } else if (week_num === 3) {
+      //setTaskOfListThird(tasks);
+    }
   };
 
   useEffect(() => {
-    getTaskData();
+    getTaskData(1);
+    getTaskData(2);
   }, []);
 
   return (
     <Wrapper>
-      <TaskMenuTable week="1" value={taskOfList}></TaskMenuTable>
+      <TaskMenuTable week="1" value={taskOfListFirst}></TaskMenuTable>
+      <TaskMenuTable week="2" value={taskOfListSecond}></TaskMenuTable>
     </Wrapper>
   );
 }
